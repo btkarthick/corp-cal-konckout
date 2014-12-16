b440c10856936b125b5a2aeb012502d329eea2e8
Karthick B <btkarthick@users.noreply.github.com>
2014-12-16 22:14:48 +0530

    Calendar list per button clicked done - Committed on 16 Dec 2014.

--- js/demo.js
+++ js/demo.js
@@ -7,7 +7,7 @@ var calendarViewModel = function(){
 	var self = this;
 	var date = new Date();
 	
-	self.currentDate = ko.observable(date.getMonth());
+	self.currentMonth = ko.observable(date.getMonth());
 	
 	self.currentYear = date.getFullYear();
 	
@@ -16,7 +16,6 @@ var calendarViewModel = function(){
 	
 	self.allYearsList = ["2013","2014","2015","2016"];
 	
-
 	self.currentCalendar = function(){
 		
 		var objDays = [];
@@ -26,7 +25,7 @@ var calendarViewModel = function(){
 		if(!$.isEmptyObject(objMonthList))
 				
 		{
-			var currentListing = objMonthList[self.currentDate()].Days;
+			var currentListing = objMonthList[self.currentMonth()].Days;
 		
 			currentListing = currentListing.split(",");
 		
@@ -40,15 +39,13 @@ var calendarViewModel = function(){
 	
 		return(objDays);
 	};
-	
-	
-	
 }
-
 
 // Create the object for our ViewModel
 
 var calView = new calendarViewModel();
+
+// End of oject creation
 
 
 var getCalendarData = function(){
@@ -95,16 +92,114 @@ var calendarInit = function(){
 	
 	getCalendarData(calView);
 	
-	$("#export_pop").on("click" , function(){
+	/*$("#export_pop").on("click" , function(){
 		
-		calView.currentDate('10');
+		calView.currentMonth('10');
 		
 		$("#sel_month").selectmenu('refresh', true);
 		
-	});
+	});*/
 	
 }
 
 
 
 $(document).on("ready" , calendarInit);
+
+/* KO's custom binding for previous button
+ * 
+ * Wll be call each month change from - Selectbox, buttons click
+ */ 
+
+ko.bindingHandlers.jqmPrevButton = {
+	
+	init: function(element, valueAccessor, allBindings, bindingContext){
+		
+		$(element).on("click" , function(){
+			
+			var cmonth = bindingContext.currentMonth();
+			
+			bindingContext.currentMonth(parseInt(cmonth) - 1);
+						
+		});
+		
+	} ,
+	
+	
+	update : function(element, valueAccessor, allBindings, bindingContext) {
+        
+		// This will be called once when the binding is first applied to an element,
+        // and again whenever any observables/computeds that are accessed change
+        // Update the DOM element based on the supplied values here.
+		
+		var offset = parseInt(bindingContext.currentMonth()) - 1;
+		
+		log.debug("prev " + offset);
+		
+		var liEle = $(element).parents("li.calendar_nav");
+		
+		if(offset > -1){
+				
+				var prevText = bindingContext.allMonthsList[offset].name.substring(0,3);
+				
+				$(liEle).show();
+			
+				$(element).text(prevText).button("refresh");
+			}
+		
+			else{
+				  $(liEle).hide();	
+			}
+     }
+}
+
+
+/* KO's custom binding for next button
+ * 
+ * Wll be call each month change from - Selectbox, buttons click
+ */
+
+ko.bindingHandlers.jqmNextButton = {
+	
+	
+	init: function(element, valueAccessor, allBindings, bindingContext){
+		
+		$(element).on("click" , function(){
+			
+			var cmonth = bindingContext.currentMonth();
+			
+			bindingContext.currentMonth(parseInt(cmonth) + 1);
+					
+		});
+		
+	} ,
+	
+	
+	update : function(element, valueAccessor, allBindings, bindingContext) {
+        
+		// This will be called once when the binding is first applied to an element,
+        // and again whenever any observables/computeds that are accessed change
+        // Update the DOM element based on the supplied values here.
+	
+		var offset = parseInt(bindingContext.currentMonth()) + 1;
+		
+		var liEle = $(element).parents("li.calendar_nav");
+		
+		if(offset < 12){
+			
+			var nextText = bindingContext.allMonthsList[offset].name.substring(0,3);
+
+			$(liEle).show();
+
+			$(element).text(nextText).button("refresh");
+			
+		}
+		
+		else{
+			$(liEle).hide();
+		}
+		
+    }
+
+	
+}
