ab6f847019df087696c4d3e191bc89315c37da2a
Karthick B <btkarthick@users.noreply.github.com>
2014-12-18 18:44:01 +0530

    80% of the functionality completed - Committed on 18 Dec 2014 6:42 PM.

---
+++ js/cal-view-model.js
@@ -0,0 +1,256 @@
+var	log = log4javascript.getDefaultLogger();
+
+var calendarModel = {}
+
+var calendarViewModel = function(){
+	
+	var self = this;
+	var date = new Date();
+	
+	self.allowedEventsPerCell = 3;
+	
+	self.currentMonth = ko.observable(date.getMonth());
+	
+	self.currentYear = date.getFullYear();
+	
+	self.allMonthsList =  [{"id" : "0", "name" : "January"} ,{"id" : "1", "name" : "February"},{"id" : "2", "name" : "March"},{"id" : "3", "name" : "April"},{"id" : "4", "name" : "May"},{"id" : "5", "name" : "June"},{"id" : "6", "name" : "July"},{"id" : "7", "name" : "August"},{"id" : "8", "name" : "September"},{"id" : "9", "name" : "October"},{"id" : "10", "name" : "November"},{"id" : "11", "name" : "December"}];
+	
+	self.eventsCategories = null;
+	
+	self.eventsCatIds = ko.observableArray();
+		
+	self.allYearsList = ["2013","2014","2015","2016"];
+	
+	self.currentCalendar = function(){
+		
+		var objCalendar = [];
+		
+		var objMonthList = calendarModel.Output.monthlist;
+		
+		var objEventsList = calendarModel.Output.events.year.Months;
+		
+		if(!$.isEmptyObject(objMonthList))
+				
+		{
+			var currentListing = objMonthList[self.currentMonth()].Days;
+			
+			currentListing = currentListing.split(",");
+			
+			var objCurrentMonthEvents = objEventsList[self.currentMonth()].Days;
+			
+			
+			
+			$.each(currentListing , function(i, day){
+				
+				var day_split = day.split("_");
+				
+				var objEvents = {}
+				
+				objEvents["EventDay"] = day_split[1];
+				
+				objEvents["status"] = day_split[0];
+				
+				objEvents["Events"] = [];
+				
+					if(day_split[0] === "A")
+					{
+
+						$.each(objCurrentMonthEvents , function(j , objEvent){
+
+							 if(day_split[1] === objEvent.EventDay)
+							 {
+
+
+								 if(!$.isEmptyObject(objEvent.Events))
+								 {
+									 //var counter = 0;	
+									
+									 $.each(objEvent.Events , function(k , eve){
+
+									   if(self.eventsCatIds.indexOf(eve.EventTypeId) > -1)
+									   {
+											/*counter++;
+										   
+										   if(( counter > self.allowedEventsPerCell))
+										   { return false;}*/
+										   
+										    objEvents["Events"].push(eve);
+										   
+										   
+
+										}
+
+									 });
+								  }
+
+								 else
+								{
+									objEvents["Events"] = [];
+								}
+
+							 }
+
+						});
+					
+					}
+		
+				objCalendar.push(objEvents);
+				
+			});			
+			
+		}
+	
+		return(objCalendar);
+	};
+	
+	
+	self.eventTypesTemplateAfterRender = function(elements){
+
+	};
+	
+}
+
+// Create the object for our ViewModel
+
+var calView = new calendarViewModel();
+
+// End of oject creation
+
+
+/* KO's custom binding for previous button
+ * 
+ * Wll be call each month change from - Selectbox, buttons click
+ */ 
+
+ko.bindingHandlers.jqmPrevButton = {
+	
+	init: function(element, valueAccessor, allBindings, bindingContext){
+		
+		$(element).on("click" , function(){
+			
+			var cmonth = bindingContext.currentMonth();
+			
+			bindingContext.currentMonth(parseInt(cmonth) - 1);
+						
+		});
+		
+	} ,
+	
+	
+	update : function(element, valueAccessor, allBindings, bindingContext) {
+        
+		// This will be called once when the binding is first applied to an element,
+        // and again whenever any observables/computeds that are accessed change
+        // Update the DOM element based on the supplied values here.
+		
+		var offset = parseInt(bindingContext.currentMonth()) - 1;
+		
+			
+		var liEle = $(element).parents("li.calendar_nav");
+		
+		if(offset > -1){
+				
+				var prevText = bindingContext.allMonthsList[offset].name.substring(0,3);
+				
+				$(liEle).show();
+			
+				$(element).text(prevText).button("refresh");
+			}
+		
+			else{
+				  $(liEle).hide();	
+			}
+     }
+}
+
+
+/* KO's custom binding for next button
+ * 
+ * Wll be call each month change from - Selectbox, buttons click
+ */
+
+ko.bindingHandlers.jqmNextButton = {
+	
+	
+	init: function(element, valueAccessor, allBindings, bindingContext){
+		
+		$(element).on("click" , function(){
+			
+			var cmonth = bindingContext.currentMonth();
+			
+			bindingContext.currentMonth(parseInt(cmonth) + 1);
+					
+		});
+		
+	} ,
+	
+	
+	update : function(element, valueAccessor, allBindings, bindingContext) {
+        
+		// This will be called once when the binding is first applied to an element,
+        // and again whenever any observables/computeds that are accessed change
+        // Update the DOM element based on the supplied values here.
+	
+		var offset = parseInt(bindingContext.currentMonth()) + 1;
+		
+		var liEle = $(element).parents("li.calendar_nav");
+		
+		if(offset < 12){
+			
+			var nextText = bindingContext.allMonthsList[offset].name.substring(0,3);
+
+			$(liEle).show();
+
+			$(element).text(nextText).button("refresh");
+			
+		}
+		
+		else{
+			$(liEle).hide();
+		}
+		
+    }
+
+	
+}
+
+
+ko.bindingHandlers.jqmChecked = {
+    init: function (element, valueAccessor, allBindings, bindingContext) {
+		
+		var eveCatID = ko.unwrap(valueAccessor());
+		
+		$(element).on("click" , function(){
+			
+				log.fatal(ko.toJSON(calView.eventsCatIds()));
+			
+				if($(this).is(":checked"))
+				{
+					calView.eventsCatIds.push(eveCatID);
+				}
+			
+				else
+				{
+					calView.eventsCatIds.remove(eveCatID);
+				}
+			
+		});
+	},
+	
+    update: function (element, valueAccessor) {
+       
+		//$(element).checkboxradio("refresh");
+    }
+}
+
+ko.bindingHandlers.eventHideShow = {
+	
+	update : function(element, valueAccessor , allBindings, bindingContext){
+		
+		var currentIndex = parseInt(ko.unwrap(valueAccessor()));
+		
+		if(currentIndex > (calView.allowedEventsPerCell - 1))
+			$(element).hide();
+	} 
+	
+}
--- js/demo.js
+++ js/demo.js
@@ -1,205 +1,53 @@
-var	log = log4javascript.getDefaultLogger();
+var calendarDataSuccess = function(data){
+	
+	if(!$.isEmptyObject(data))
+	{
+		calendarModel = data;
 
-var calendarModel = {}
-
-var calendarViewModel = function(){
-	
-	var self = this;
-	var date = new Date();
-	
-	self.currentMonth = ko.observable(date.getMonth());
-	
-	self.currentYear = date.getFullYear();
-	
-	self.allMonthsList =  [{"id" : "0", "name" : "January"} ,{"id" : "1", "name" : "February"},{"id" : "2", "name" : "March"},{"id" : "3", "name" : "April"},{"id" : "4", "name" : "May"},{"id" : "5", "name" : "June"},{"id" : "6", "name" : "July"},{"id" : "7", "name" : "August"},{"id" : "8", "name" : "September"},{"id" : "9", "name" : "October"},{"id" : "10", "name" : "November"},{"id" : "11", "name" : "December"}];
-	
-	
-	self.allYearsList = ["2013","2014","2015","2016"];
-	
-	self.currentCalendar = function(){
+		calView.eventsCategories = calendarModel.Output.eventtypelist;
 		
-		var objDays = [];
+		getEventsCategoryIDs();
 		
-		var objMonthList = calendarModel.Output.monthlist;
+		ko.applyBindings(calView);
 		
-		if(!$.isEmptyObject(objMonthList))
 				
-		{
-			var currentListing = objMonthList[self.currentMonth()].Days;
+		$("#primary-left ul").listview("refresh");
 		
-			currentListing = currentListing.split(",");
-		
-			$.each(currentListing , function(i, day){
-				
-				var day_split = day.split("_");
-				
-					objDays.push({"status" : day_split[0] , "day" : day_split[1]});
-				});
-		}
-	
-		return(objDays);
-	};
+		$("#primary-left ul").find("INPUT[type='checkbox']").prop("checked" , true);
+	}
 }
 
-// Create the object for our ViewModel
+var getEventsCategoryIDs = function(){
+	
+	var arrIDs = [];
+	
+	$.each(calView.eventsCategories , function(i , obj){
+		
+		calView.eventsCatIds.push(obj.Guid);
+				
+	});
+}
 
-var calView = new calendarViewModel();
-
-// End of oject creation
+var calendarDataError = function(jqXHR, textStatus, errorThrown){
+	
+	log.fatal(errorThrown);
+	log.error(errorThrown);
+	log.debug(jqXHR);
+}
 
 
 var getCalendarData = function(){
 	
 	var URL = "JSON/" + calView.currentYear + ".json";
 	
-	$.ajax(URL , {
-		
-			dataType : "json" ,
-		
-			type : "GET",
-		
-			success : function(data){
-				
-				
-				if(!$.isEmptyObject(data))
-				{
-					calendarModel = data;
-					
-					ko.applyBindings(calView);
-			
-				}
-				
-				
-					
-			},
-		
-			error: function(jqXHR, textStatus, errorThrown){
-				
-				log.fatal(errorThrown);
-				log.error(errorThrown);
-				log.debug(jqXHR);
-			}
-		
-		
-		
-	});
-	
+	$.ajax(URL , { success : calendarDataSuccess, error: calendarDataError});
 }
-
 
 
 var calendarInit = function(){
-	
-	getCalendarData(calView);
-	
-	/*$("#export_pop").on("click" , function(){
-		
-		calView.currentMonth('10');
-		
-		$("#sel_month").selectmenu('refresh', true);
-		
-	});*/
-	
-}
 
+	getCalendarData(calView);
+}
 
 
 $(document).on("ready" , calendarInit);
-
-/* KO's custom binding for previous button
- * 
- * Wll be call each month change from - Selectbox, buttons click
- */ 
-
-ko.bindingHandlers.jqmPrevButton = {
-	
-	init: function(element, valueAccessor, allBindings, bindingContext){
-		
-		$(element).on("click" , function(){
-			
-			var cmonth = bindingContext.currentMonth();
-			
-			bindingContext.currentMonth(parseInt(cmonth) - 1);
-						
-		});
-		
-	} ,
-	
-	
-	update : function(element, valueAccessor, allBindings, bindingContext) {
-        
-		// This will be called once when the binding is first applied to an element,
-        // and again whenever any observables/computeds that are accessed change
-        // Update the DOM element based on the supplied values here.
-		
-		var offset = parseInt(bindingContext.currentMonth()) - 1;
-		
-		log.debug("prev " + offset);
-		
-		var liEle = $(element).parents("li.calendar_nav");
-		
-		if(offset > -1){
-				
-				var prevText = bindingContext.allMonthsList[offset].name.substring(0,3);
-				
-				$(liEle).show();
-			
-				$(element).text(prevText).button("refresh");
-			}
-		
-			else{
-				  $(liEle).hide();	
-			}
-     }
-}
-
-
-/* KO's custom binding for next button
- * 
- * Wll be call each month change from - Selectbox, buttons click
- */
-
-ko.bindingHandlers.jqmNextButton = {
-	
-	
-	init: function(element, valueAccessor, allBindings, bindingContext){
-		
-		$(element).on("click" , function(){
-			
-			var cmonth = bindingContext.currentMonth();
-			
-			bindingContext.currentMonth(parseInt(cmonth) + 1);
-					
-		});
-		
-	} ,
-	
-	
-	update : function(element, valueAccessor, allBindings, bindingContext) {
-        
-		// This will be called once when the binding is first applied to an element,
-        // and again whenever any observables/computeds that are accessed change
-        // Update the DOM element based on the supplied values here.
-	
-		var offset = parseInt(bindingContext.currentMonth()) + 1;
-		
-		var liEle = $(element).parents("li.calendar_nav");
-		
-		if(offset < 12){
-			
-			var nextText = bindingContext.allMonthsList[offset].name.substring(0,3);
-
-			$(liEle).show();
-
-			$(element).text(nextText).button("refresh");
-			
-		}
-		
-		else{
-			$(liEle).hide();
-		}
-		
-    }
-
-	
-}
